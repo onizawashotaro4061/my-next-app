07bbac7efef99330f0b630c8960393ac34f66675
onizawashotaro4061 <shotaro10072003@gmail.com>
2024-10-27 18:00:41 +0900

    detail

--- src/app/components/ClickButton.tsx
+++ src/app/components/ClickButton.tsx
@@ -1,29 +1,38 @@
 "use client";
-import { useState } from 'react';
 
-const ClickButton = () => {
-  const [clickCount, setClickCount] = useState(0);
+interface ClickButtonProps {
+  onClick: () => void;
+  count: number;
+  label: string; // フロントエンドでの表示用
+  color: string;
+}
 
+const ClickButton: React.FC<ClickButtonProps> = ({ onClick, label, color }) => {
   const handleClick = async () => {
-    try {
-      const response = await fetch('/api/click', { // エンドポイントの確認
-        method: 'POST',
-      });
+    const course = label === "魔術師" ? "A" : "B"; // labelからcourseに変換
 
-      if (response.ok) {
-        setClickCount(clickCount + 1);
-      } else {
-        console.error('クリック数の記録に失敗しました。');
-      }
-    } catch (error) {
-      console.error('エラー:', error);
+    const res = await fetch('/api/click', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ course }), // labelではなくcourseを送信
+    });
+    const data = await res.json();
+    
+    if (res.ok) {
+      onClick(); // クリック数を更新する関数を呼び出す
+    } else {
+      console.error('Error:', data.error);
     }
   };
 
   return (
-    <button onClick={handleClick}>
-      クリック数: {clickCount}
-    </button>
+    <div>
+      <button  onClick={handleClick} 
+        style={{ backgroundColor: color, color: '#fff', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>
+          {label}</button>
+    </div>
   );
 };
 
